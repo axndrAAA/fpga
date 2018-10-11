bf05bceede37d558775af0f370da71814cd96d1f
Alexander <axndr-AAA@yandex.ru>
2018-10-11 15:07:23 +0300

    Написан тест с различными значениями посылки.В тесте по преженему проблеммы с отслеживанием data_rdy. reset работает не корректно, после него все рушится.

--- Serial2Paralel/ver1/src/TestBench_rs_in/rs_in_TB.vhd
+++ Serial2Paralel/ver1/src/TestBench_rs_in/rs_in_TB.vhd
@@ -24,12 +24,27 @@ architecture TB_ARCHITECTURE of rs_in_tb is
 	
 	-- Stimulus signals - signals mapped to the input and inout ports of tested entity
 	signal clk : STD_LOGIC:='0';
-	signal reset : STD_LOGIC;
+	signal reset : STD_LOGIC:= '0';
 	signal uart_in : STD_LOGIC:='1';
 	-- Observed signals - signals mapped to the output ports of tested entity
 	signal data_out : STD_LOGIC_VECTOR(7 downto 0);
 	signal data_rdy : STD_LOGIC;
-	signal test_out1	:  std_logic_vector(1 downto 0);
+	signal test_out1	:  std_logic_vector(1 downto 0);   
+	signal msg_changed : STD_LOGIC:='0';
+	
+	--оправляемое сообщение
+	signal  test_msg : std_logic_vector(7 downto 0):= x"4C";
+	
+function to_string ( a: std_logic_vector) return string is
+	variable b : string (1 to a'length) := (others => NUL);
+	variable stri : integer := 1; 
+begin
+    for i in a'range loop
+        b(stri) := std_logic'image(a((i)))(2);
+    stri := stri+1;
+    end loop;
+return b;
+end function;
 
 	--процедура передачи сообщения на uart_in
 	procedure UART_WRITE_BYTE (
@@ -69,34 +84,42 @@ begin
 	-- Add your stimulus here ...
 	
 	clk	<= not clk after 5 ns; 		   
-	
+	reset <= not reset after 170000 ns;
 process is
 begin		   
---	wait for 2000 ns;
---	UART_WRITE_BYTE(B"10110101", uart_in);	 
---	wait for 20 ms;	 	 
-
---отправка сообщения 	 
-	wait for 2000 ns;
-    wait until rising_edge(clk);
-    UART_WRITE_BYTE(x"4C", uart_in);
-     
+	wait for 2000 ns; 
 	
-	
-	-- проверка принятого сообщения	  
-	--wait until rising_edge(data_rdy);	
-	if(rising_edge(data_rdy))then
-	
-	end if;
-	
-    	if (data_out = x"4C") then
-      		report "Тест пройден. Принят корректный байт" severity note;
+----отправка сообщения 	 
+--    UART_WRITE_BYTE(x"4C", uart_in);
+----	wait for 20 ms;	     
+--	
+--	
+---- проверка принятого сообщения	  
+----	if(data_rdy = '1')then
+----	end if;
+--	    if (data_out = x"4C") then
+--      		report "Тест пройден. Принят корректный байт" severity note;
+--   		else
+--      		report "Тест не пройден. Принят не корректный байт" severity note;
+--    	end if;
+	 
+	 
+	 for i in 0 to 5 loop
+		test_msg(i) <= '1';
+		msg_changed <= not msg_changed;	 
+		wait for 2000 ns;
+		UART_WRITE_BYTE(test_msg, uart_in);
+		report "Отправлен байт " & to_string(test_msg) severity note;
+	    if (data_out = test_msg) then
+      		report "Тест пройден. Принят корректный байт " & to_string(data_out) severity note;
    		else
-      		report "Тест не пройден. Принят не корректный байт" severity note;
+      		report "Тест не пройден. Принят не корректный байт " & to_string(data_out) severity note;
     	end if;
- 
-    assert false report "Тест завершен" severity note;	
-	
+	 end loop; 
+	 
+	 
+	 
+	 assert false report "Тест завершен" severity note;
 end process;
 
 
