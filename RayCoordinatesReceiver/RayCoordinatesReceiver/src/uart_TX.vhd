7bfb233ee0a50300bc05940ee4f8c47618f4d841
Alexander <axndr-AAA@yandex.ru>
2018-10-15 13:45:07 +0300

    Написан передатчик и болванка теста для него. Необходимо тестировать

--- RayCoordinatesReceiver/RayCoordinatesReceiver/src/uart_TX.vhd
+++ RayCoordinatesReceiver/RayCoordinatesReceiver/src/uart_TX.vhd
@@ -20,7 +20,7 @@
 
 library IEEE;
 use IEEE.std_logic_1164.all;
-
+use IEEE.STD_LOGIC_unsigned.all;
 entity uart_tx is
 	port(
 	clk			: in std_logic;  --100Mhz
@@ -33,11 +33,75 @@ entity uart_tx is
 end uart_tx;
 
 
-architecture uart_tx of uart_tx is
+architecture uart_tx of uart_tx is	
+	constant clk_per_bit	: std_logic_vector(15 downto 0):=x"0361"; -- число тактов на каждый бит
+	type stm_states is (
+		waitData, -- начальное состояние. Ожидание данных на входе
+		buferingData, -- сохранение данных на входной шине во внутренний буфер
+		txStartBit, -- передача стартового бита
+		txData, -- передача данных 
+		txStopBit -- передача стопового бита
+	);
+	
+	signal st_main			:	stm_states:= waitData; -- переменная состояния конечного автомата
+	signal tx_bit_index 	:	integer range 0 to 7:=0; -- счетчик переданного бита
+	signal input_data_bufer	: 	std_logic_vector(7 downto 0):=(others => '0'); -- входной буфер
+	signal clk_bit_counter	: 	std_logic_vector(15 downto 0); -- счетчик отсчета тактового сигнала при передаче бита
+	
+	
 begin
+  	main_pr : process(clk) 
+	  begin
+	  	if (rising_edge(clk)) then
+			if(reset = '1')  then
+				st_main	<= waitData;
+			end if;
+			
+			case st_main is				
+				when waitData => -- режим ожидания данных на входе
+					uart_out <= '1'; --	подтягиваем выход к логической 1   
+					tx_bit_index <= 0; -- обнуляем счетчик переданных бит
+					if (data_in_rdy = '1')then -- если на входной шине есть валидные данные
+						st_main <= buferingData; -- переходим к буферизации данных
+					end if;	
 
-	-- enter your statements here --
-	
-	
+				when buferingData =>
+					input_data_bufer <= data_in;
+					st_main <= txStartBit;				
+				when txStartBit =>
+					uart_out <= '0'; -- устанавливаем лог.0 на выход
+					if(clk_bit_counter < clk_per_bit)then -- отсчитываем длительность бита
+						clk_bit_counter <= clk_bit_counter + '1';
+					else
+						clk_bit_counter <=(others=>'0');-- если отсчитали, сбрасываем счетчик, и
+						st_main <= txData; -- переходим к передаче битов данных
+					end if;
+				when txData => 
+					uart_out <= input_data_bufer(tx_bit_index); -- записываем передаваемый бит на выходную линию
+					if(clk_bit_counter < clk_per_bit)then -- отсчитываем длительность бита 
+						clk_bit_counter <= clk_bit_counter + '1';
+					else 					
+						clk_bit_counter <=(others=>'0');-- если отсчитали, сбрасываем счетчик
+						if (tx_bit_index < 7)then -- и проверяем не закончена ли передача посылки
+							tx_bit_index <=	 tx_bit_index + 1; -- если не закончена, переходим к передаче следующего бита
+						else
+							st_main <= txStopBit; -- если закончена, переходим к передаче стопового бита
+						end if;	
+					end if;
+				
+				when txStopBit =>
+					uart_out <= '1';
+					if(clk_bit_counter < clk_per_bit)then -- отсчитываем длительность бита
+						clk_bit_counter <= clk_bit_counter + '1';
+					else
+						clk_bit_counter <=(others=>'0');-- если отсчитали, сбрасываем счетчик
+						st_main <= waitData; -- и переходим к ожиданию следующей посылки
+					end if;							
+				when others => 
+					st_main <= waitData;
+				end case;
+		end if;		  
+	end process main_pr;   
+		
 
 end uart_tx;
